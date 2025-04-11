4f3c3e701a1aa11d690ac47e22f984e75e9e8e60
Osman Çelik <osmanceliktr@gmail.com>
2025-04-11 19:18:50 +0300

    Remove unnecessary SQL settings and comments from db.sql

--- db.sql
+++ db.sql
@@ -2,17 +2,7 @@
 -- Sunucu:                       127.0.0.1
 -- Sunucu sürümü:                8.2.0 - MySQL Community Server - GPL
 -- Sunucu İşletim Sistemi:       Win64
--- HeidiSQL Sürüm:               12.6.0.6765
 -- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- tablo yapısı dökülüyor osmancelikdb.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -23,13 +13,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin5;
 
--- osmancelikdb.users: ~1 rows (yaklaşık) tablosu için veriler indiriliyor
+-- osmancelikdb.users: ~1 rows (yaklaşık) tablosu için veriler indiriliyor şifre:pratik
 DELETE FROM `users`;
 INSERT INTO `users` (`id`, `username`, `nameSurname`, `password`) VALUES
 	(1, 'admin', 'Admin', '$2y$10$rzQuKck/tSciegxcOCSap.TpCSF/XoqwXhAHxSOfwWA1bkD4Wk1U6');
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+
